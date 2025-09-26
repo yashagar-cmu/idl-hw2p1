272ccaee0fe4bab490fba7905289479b01dfd5d1
Yash Agarwal <yashagar@andrew.cmu.edu>
2025-09-26 01:47:16 -0400

    resampling.py

--- mytorch/nn/resampling.py
+++ mytorch/nn/resampling.py
@@ -4,16 +4,27 @@ import numpy as np
 class Upsample1d():
     def __init__(self, upsampling_factor):
         self.upsampling_factor = upsampling_factor
+        self.index_mapper = lambda i : i*self.upsampling_factor
 
-    def forward(self, A):
+    def get_indices(self, in_shape):
+        original_indices = np.arange(in_shape)
+        final_indices = self.index_mapper(original_indices).astype(int)
+        return final_indices
+
+    def forward(self, A: np.ndarray):
         """
         Argument:
             A (np.array): (batch_size, in_channels, input_width)
         Return:
             Z (np.array): (batch_size, in_channels, output_width)
         """
-        Z = None  # TODO
-        return NotImplemented
+        bs, inc, w_in = A.shape
+        w_out = self.upsampling_factor*(w_in-1) + 1
+
+        Z = np.zeros((bs, inc, w_out), dtype = A.dtype)
+        Z[..., self.get_indices(w_in)] = A
+
+        return Z
 
     def backward(self, dLdZ):
         """
@@ -22,13 +33,13 @@ class Upsample1d():
         Return:
             dLdA (np.array): (batch_size, in_channels, input_width)
         """
-        dLdA = None  # TODO
-        return NotImplemented
+        return dLdZ[..., ::self.upsampling_factor]
 
 
 class Downsample1d():
     def __init__(self, downsampling_factor):
         self.downsampling_factor = downsampling_factor
+        self.w_in = None
 
     def forward(self, A):
         """
@@ -37,8 +48,13 @@ class Downsample1d():
         Return:
             Z (np.array): (batch_size, in_channels, output_width)
         """
-        Z = None  # TODO
-        return NotImplemented
+        _, _, self.w_in = A.shape
+
+        # w_out = w_in//self.downsampling_factor + (1 if w_in%self.downsampling_factor!=0 else 1)
+
+        Z = A[..., ::self.downsampling_factor]
+        return Z
+
 
     def backward(self, dLdZ):
         """
@@ -47,23 +63,38 @@ class Downsample1d():
         Return:
             dLdA (np.array): (batch_size, in_channels, input_width)
         """
-        dLdA = None  # TODO
-        return NotImplemented
+        assert self.w_in is not None
+        bs, inc, _ = dLdZ.shape
 
+        upsampled = Upsample1d(self.downsampling_factor).forward(dLdZ)
+
+        dLdA = np.zeros((bs, inc, self.w_in), dtype=dLdZ.dtype)
+        dLdA[..., :upsampled.shape[-1]] = upsampled
+        return dLdA
 
 class Upsample2d():
     def __init__(self, upsampling_factor):
         self.upsampling_factor = upsampling_factor
+        self.up1d = Upsample1d(upsampling_factor)
 
-    def forward(self, A):
+    def forward(self, A: np.ndarray):
         """
         Argument:
             A (np.array): (batch_size, in_channels, input_height, input_width)
         Return:
             Z (np.array): (batch_size, in_channels, output_height, output_width)
         """
-        Z = None  # TODO
-        return NotImplemented
+        bs, inc, hin, win = A.shape
+
+        hout = self.upsampling_factor*(hin-1) + 1
+        wout = self.upsampling_factor*(win-1) + 1
+
+        Z = np.zeros(shape=(bs, inc, hout, wout), dtype = A.dtype)
+        print(f"{self.up1d.get_indices(hin)=}, {self.up1d.get_indices(win)=}")
+        Z[..., self.up1d.get_indices(hin)[:, None], self.up1d.get_indices(win)[None, :]] = A
+        
+        return Z
+
 
     def backward(self, dLdZ):
         """
@@ -72,13 +103,14 @@ class Upsample2d():
         Return:
             dLdA (np.array): (batch_size, in_channels, input_height, input_width)
         """
-        dLdA = None  # TODO
-        return NotImplemented
+        return dLdZ[..., ::self.upsampling_factor, ::self.upsampling_factor]  # TODO
 
 
 class Downsample2d():
     def __init__(self, downsampling_factor):
         self.downsampling_factor = downsampling_factor
+        self.hin = None
+        self.win = None
 
     def forward(self, A):
         """
@@ -87,9 +119,8 @@ class Downsample2d():
         Return:
             Z (np.array): (batch_size, in_channels, output_height, output_width)
         """
-        Z = None  # TODO
-
-        return NotImplemented
+        self.hin, self.win = A.shape[-2:]
+        return A[..., ::self.downsampling_factor, ::self.downsampling_factor]
 
     def backward(self, dLdZ):
         """
@@ -98,5 +129,13 @@ class Downsample2d():
         Return:
             dLdA (np.array): (batch_size, in_channels, input_height, input_width)
         """
-        dLdA = None  # TODO
-        return NotImplemented
+        assert self.hin is not None and self.win is not None
+
+        bs, inc, _, _ = dLdZ.shape
+
+        dLdA = np.zeros(shape=(bs, inc, self.hin, self.win), dtype = dLdZ.dtype)
+
+        upped = Upsample2d(self.downsampling_factor).forward(dLdZ)
+        dLdA[..., :upped.shape[-2], :upped.shape[-1]] = upped
+
+        return dLdA
--- sandbox/resampling_sandbox.py
+++ sandbox/resampling_sandbox.py
@@ -3,17 +3,17 @@ import sys
 
 import numpy as np
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # Absolute path to the root directory
-mytorch_dir = os.path.join(project_root, 'mytorch')
-mytorch_nn_dir = os.path.join(mytorch_dir, 'nn')
-models_dir = os.path.join(project_root, 'models')
+# project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # Absolute path to the root directory
+# mytorch_dir = os.path.join(project_root, 'mytorch')
+# mytorch_nn_dir = os.path.join(mytorch_dir, 'nn')
+# models_dir = os.path.join(project_root, 'models')
 
-sys.path.append(mytorch_dir)
-sys.path.append(mytorch_nn_dir)
-sys.path.append(models_dir)
+# sys.path.append(mytorch_dir)
+# sys.path.append(mytorch_nn_dir)
+# sys.path.append(models_dir)
 
-from flatten import *
-from resampling import *
+from mytorch.flatten import *
+from mytorch.nn.resampling import *
 
 # Note: Feel free to change anything about this file for your testing purposes
 
@@ -23,35 +23,92 @@ np.random.seed(11485) # Set the seed so that the random values are the same each
 in_c, out_c = 2, 2
 batch, input_width = 1, 4
 kernel = 2
-x = np.random.randn(batch, in_c, input_width)
-
-print("input shape: ", x.shape)
-print("input: ", x)
-
-# Initialize the resampling class
-upsampling_factor = 2
-upsample_1d = Upsample1d(upsampling_factor)
 
 # TODO: Uncomment the following lines and change the file to test downsampling
-#downsampling_factor = 2
-#downsample_1d = Downsample1d(downsampling_factor)
+
 ########################################################################
 
 # TODO: Uncomment the following lines and change the file to 2D versions
-# x = np.random.randn(batch, in_c, input_width, input_width)
-# upsample_2d = Upsample2d(upsampling_factor)
+
 # downsample_2d = Downsample2d(downsampling_factor)
 ########################################################################
 
 #TODO: Uncomment and/or add print statements as you need them.
 
 # Perform forward and backward pass
-forward_res = upsample_1d.forward(x)
+
+# Initialize the resampling class
+# upsampling_factor = 2
+# upsample_1d = Upsample1d(upsampling_factor)
+
+# x = np.random.randn(batch, in_c, input_width)
+
+# print("input shape: ", x.shape)
+# print("input: ", x)
+
+# forward_res = upsample_1d.forward(x)
 
 # print("Forward shape: ", forward_res.shape)
 # print("Forward result: ", forward_res)
 
-backward_res = upsample_1d.backward(forward_res)
+# backward_res = upsample_1d.backward(forward_res)
 
 # print("Backward shape: ", backward_res.shape)
 # print("Backward result: ", backward_res)
+
+# assert np.array_equal(backward_res, x)
+
+## DOWNSAMPLE
+
+#downsampling_factor = 3
+#downsample_1d = Downsample1d(downsampling_factor)
+
+# forward_res = downsample_1d.forward(x)
+
+# print("Forward shape: ", forward_res.shape)
+# print("Forward result: ", forward_res)
+
+# backward_res = downsample_1d.backward(forward_res)
+
+# print("Backward shape: ", backward_res.shape)
+# print("Backward result: ", backward_res)
+
+## UPSAMPLING 2D
+
+# upsampling_factor = 2
+# x = np.random.randn(batch, in_c, input_width, input_width)
+# print("input shape: ", x.shape)
+# print("input: ", x)
+
+# upsample_2d = Upsample2d(upsampling_factor)
+
+# forward_res = upsample_2d.forward(x)
+
+# print("Forward shape: ", forward_res.shape)
+# print("Forward result: ", forward_res)
+
+# backward_res = upsample_2d.backward(forward_res)
+
+# print("Backward shape: ", backward_res.shape)
+# print("Backward result: ", backward_res)
+
+# assert np.array_equal(backward_res, x)
+
+## DOWNSAMPLING 2D
+
+upsampling_factor = 3
+x = np.random.randn(batch, in_c, input_width, input_width)
+print("input shape: ", x.shape)
+print("input: ", x)
+
+downsample_2d = Downsample2d(upsampling_factor)
+
+forward_res = downsample_2d.forward(x)
+
+print("Forward shape: ", forward_res.shape)
+print("Forward result: ", forward_res)
+
+backward_res = downsample_2d.backward(forward_res)
+
+print("Backward shape: ", backward_res.shape)
+print("Backward result: ", backward_res)
